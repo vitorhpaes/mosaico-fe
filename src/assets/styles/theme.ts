aa43540efc02b76ca2dcf7093d55b365196206d0
Vítor Paes <vitor.paes@compasso.com.br>
2022-09-08 17:08:32 -0300

    Setup theme, image urls and import logo

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,31 @@
+const theme = {
+    colors: {
+        background: {
+            paper: '',
+            default: '',
+        },
+        font: {
+            main: '',
+            disabled: '',
+            reverse: '',
+        },
+        primary: {
+            main: '',
+        },
+        secondary: {
+            main: '',
+        },
+        success: {
+            main: '',
+        },
+        danger: {
+            main: '',
+        },
+        warning: {
+            main: '',
+        },
+        info: {
+            main: '',
+        },
+    },
+}
