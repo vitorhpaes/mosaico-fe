1c5369179248c250aa6eabb8e8758332d1e5c544
Vítor Paes <vitor.paes@compasso.com.br>
2022-09-08 16:54:21 -0300

    Setup router, redux and services

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { QueryClientProvider } from 'react-query'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
+import LayoutProvider from './components/LayoutProvider'
+import Home from './pages/Home'
+import queryClient from './services/queryClient'
 
 const App: React.FC = () => {
-    return <div>oi</div>
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LayoutProvider />}>
+                        <Route index element={<Home />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    )
 }
 
 export default App
