1899763c0cab039244576db0728ea5c9594627f6
Vítor Paes <vitor.paes@compasso.com.br>
2022-09-09 15:32:52 -0300

    Add Page Container and default texts at homepageg

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import withLayoutConfig from '../withLayoutConfig';
-import { Typography } from '@ds';
+import React from 'react'
+import withLayoutConfig from '../withLayoutConfig'
+import { Typography } from '@ds'
+
+import PageContainer from '@components/Layout/PageContainer'
 
 const Home: React.FC = () => {
-  return <Typography variant='title'>homepage</Typography>
+    return (
+        <PageContainer>
+            <Typography variant="subtitle">Hi, Andrea</Typography>
+            <Typography variant="title" size={24} transform={'none'}>
+                What are you looking for today?
+            </Typography>
+        </PageContainer>
+    )
 }
 
-export default withLayoutConfig(<Home />);
+export default withLayoutConfig(<Home />)
