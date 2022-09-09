83f21648c3128356ef35e93ac75f98c229000cb5
Vítor Paes <vitor.paes@compasso.com.br>
2022-09-09 17:33:25 -0300

    Fix font imports

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -2,14 +2,26 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
 
-    @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
-
-    * {
+    *, body {
         box-sizing: border-box;
         font-family: 'DM Sans', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         letter-spacing: 0.2px;
+    }
+    
+    ::placeholder,
+    ::-webkit-input-placeholder {
+        color: ${({ theme }) => theme.palette.grey.index};
+    }
+    :-ms-input-placeholder {
+        color: ${({ theme }) => theme.palette.grey.index};
+    }
+
+    input, textarea {
+        &:focus {
+            outline:none
+        }
     }
 
     body {
