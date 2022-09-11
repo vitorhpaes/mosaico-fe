1e3cc54c8f9b9c2b55a710e3607b4de832a8b33d
Vítor Paes <vitor.paes@compasso.com.br>
2022-09-11 00:47:29 -0300

    Add reviews section and finish styling

--- src/pages/Product/ReviewsSection/index.tsx
+++ src/pages/Product/ReviewsSection/index.tsx
@@ -5,8 +5,9 @@ import SectionTitle from '@components/SectionTitle'
 
 import { NormalizedReview } from '@services/normalizers/products'
 import styled from 'styled-components'
-import { Avatar, Typography } from '@ds'
+import { Avatar, Rating, Spacing, Typography } from '@ds'
 import { dateFromNow } from '@helpers/date'
+import { randomizeInt } from './../../../helpers/randomize'
 
 interface ReviewsSectionProps {
     reviews: {
@@ -18,33 +19,58 @@ interface ReviewsSectionProps {
 const StyledReviewSectionContainer = styled(SectionContainer)`
     display: flex;
     flex-direction: column;
-    gap: ${({ theme }) => theme.applySpace('standard')};
+    gap: ${({ theme }) => theme.applySpace('large')};
 `
 
 const StyledReviewCard = styled.div`
     display: flex;
-    flex-direction: column;
+    width: 100%;
+    gap: ${({ theme }) => theme.applySpace('medium')};
 `
-const StyledReviewCardHeader = styled.div`
+const StyledReviewCardContent = styled.div`
     display: flex;
+    width: 100%;
+    flex-direction: column;
 
     .name-and-time {
         width: 100%;
         display: flex;
+        flex-direction: row;
         align-items: center;
         justify-content: space-between;
     }
 `
 
+const StyledReviewPictureCard = styled.div`
+    border: 1px solid ${({ theme }) => theme.palette.grey.main};
+    border-radius: ${({ theme }) => theme.applySpace('medium')};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: ${({ theme }) => theme.applySpace('huge')};
+    height: ${({ theme }) => theme.applySpace('huge')};
+
+    overflow: hidden;
+
+    img {
+        width: auto;
+        height: auto;
+        max-width: 100%;
+        max-height: 100%;
+    }
+`
+
 const ReviewsSection: React.FC<ReviewsSectionProps> = ({ reviews }) => {
-    console.log({ reviews })
     return (
         <StyledReviewSectionContainer color="paper" padding={0} mt="standard">
             <SectionTitle title={`Review (${reviews.count})`} />
             {reviews.data.map((review) => (
                 <StyledReviewCard>
-                    <StyledReviewCardHeader>
+                    <div>
                         <Avatar src={review.avatar} size="largeXX" />
+                    </div>
+                    <StyledReviewCardContent>
                         <div className="name-and-time">
                             <Typography variant="subtitle">
                                 {review.consumer}
@@ -53,7 +79,24 @@ const ReviewsSection: React.FC<ReviewsSectionProps> = ({ reviews }) => {
                                 {dateFromNow(review.date)}
                             </Typography>
                         </div>
-                    </StyledReviewCardHeader>
+                        <Rating value={review.rating} />
+                        <Spacing mt="standard">
+                            <Typography variant="body1">
+                                {review.review}
+                            </Typography>
+                        </Spacing>
+                        {review.images && (
+                            <Spacing mt="standard">
+                                {review.images.map((image) => (
+                                    <Spacing mr="standard">
+                                        <StyledReviewPictureCard>
+                                            <img src={image} />
+                                        </StyledReviewPictureCard>
+                                    </Spacing>
+                                ))}
+                            </Spacing>
+                        )}
+                    </StyledReviewCardContent>
                 </StyledReviewCard>
             ))}
         </StyledReviewSectionContainer>
