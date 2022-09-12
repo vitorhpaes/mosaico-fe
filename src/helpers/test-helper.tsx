9595fe846ba476ee68c8e6b6400b7b3269b7a63f
Vítor Paes <vitor.paes@compasso.com.br>
2022-09-12 02:25:08 -0300

    Add global styles at test provider

--- src/helpers/test-helper.tsx
+++ src/helpers/test-helper.tsx
@@ -7,6 +7,7 @@ import { store } from '@app/state/store'
 
 import { QueryClientProvider } from 'react-query'
 import queryClient from '@services/queryClient'
+import GlobalStyle from '@app/assets/styles/GlobalStyles'
 
 interface WrapperProps {
     children: React.ReactNode
@@ -16,7 +17,10 @@ export const TestElementWrapper: React.FC<WrapperProps> = ({ children }) => {
     return (
         <Provider store={store}>
             <QueryClientProvider client={queryClient}>
-                <ThemeProvider theme={theme}>{children}</ThemeProvider>
+                <ThemeProvider theme={theme}>
+                    <GlobalStyle />
+                    {children}
+                </ThemeProvider>
             </QueryClientProvider>
         </Provider>
     )
